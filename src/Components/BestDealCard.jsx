5aced43cf907f779e5de8221a4a2141b0296c4ca
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2026-04-12 14:21:11 +0530

    updated code

--- src/Components/BestDealCard.jsx
+++ src/Components/BestDealCard.jsx
@@ -3,8 +3,8 @@ import bestDelPhoneImage from "../assets/bestDealPhoneImage.svg";
 
 export default function BestDealCard() {
   return (
-    <div className="flex flex-col w-[20.2rem] p-2">
-      <div className="bg-[#ECEDEF] flex justify-center rounded-3xl py-7 px-3 relative">
+    <div className="flex flex-col gap-y-2 w-[19.8rem] p-2">
+      <div className="bg-[#ECEDEF] flex !outline-[8px] !outline-[#FAFAFA] justify-center rounded-3xl py-7 relative">
         <div className="absolute bg-[#FCBD01] text-white px-[1.1rem] rounded-[50%_0%] left-0 top-0 text-xs font-semibold py-3">
           New
         </div>
@@ -13,9 +13,9 @@ export default function BestDealCard() {
       <div className="text-xl font-bold">
         ADIDAS 4DFWD X PARLEY RUNNING SHOES
       </div>
-      <div className="flex gap-x-2 justify-center bg-[#232321] text-white font-semibold px-10 rounded-md py-2">
+      <button className="flex cursor-pointer gap-x-2 justify-center bg-[#232321] text-white font-semibold px-10 rounded-md text-sm py-2">
         BUY NOW - <div className="text-[#FFA52F]">₹125</div>
-      </div>
+      </button>
     </div>
   );
 }
--- src/Components/DealOfTheDay.jsx
+++ src/Components/DealOfTheDay.jsx
@@ -6,7 +6,7 @@ export default function DealOfTheDay() {
     <>
       <div className="mx-auto mb-5">
         <div className="flex items-center mb-5 justify-between mx-16">
-          <div className="font-bold uppercase text-[clamp(1rem,calc(1.7vw+0.5rem),3rem)]">
+          <div className="font-bold uppercase text-[#232321] text-[clamp(1rem,calc(1.7vw+0.5rem),3rem)]">
             Today’s Deals of the day
           </div>
           <div className="flex gap-x-2 items-center">
@@ -14,12 +14,12 @@ export default function DealOfTheDay() {
             <div className="bg-[#FCBD01] rounded-md text-sm font-bold p-2">
               16d : 21h : 57m : 23s
             </div>
-            <div className="bg-[#FCBD01] rounded-md text-xs font-semibold py-[10px] px-2">
+            <button className="bg-[#FCBD01] cursor-pointer rounded-md text-xs font-semibold py-[10px] px-2">
               VIEW ALL
-            </div>
+            </button>
           </div>
         </div>
-        <div className="flex w-fit gap-x-2 mx-auto flex-wrap">
+        <div className="flex w-fit gap-x-4 mx-auto flex-wrap">
           <BestDealCard />
           <BestDealCard />
           <BestDealCard />
